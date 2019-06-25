1ec481a86c5253c2536adcee5414c1514686dce6
Chris Mann <chrismwann@gmail.com>
2019-06-25 20:17:03 +0100

    Added bash setup and identified potential data source.

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 ls -lsha
-django-admin startproject chriswmann /usr/src/app/chriswmann
+# Run the next line to start again
+# django-admin startproject chriswmann /usr/src/app/chriswmann
+apt-get -y update && apt-get -y upgrade && apt-get -y install vim
 mv ../settings.py ./chriswmann/chriswmann/settings.py
 mv ../wsgi.py ./chriswmann/chriswmann/wsgi.py
+mv ../apps/ ./chriswmann/
+source ~/.bashrc
+python ./chriswmann/manage.py makemigrations
+python ./chriswmann/manage.py migrate
 python ./chriswmann/manage.py runserver 0.0.0.0:8000
