18b5d49ed58555b418cfc279b6ccdb8a825d9e72
Chris Mann <chrismwann@gmail.com>
2019-06-26 06:59:08 +0100

    Added non-root users and persistent analysis volume.

--- docker/analysis/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ls -lsha
-# Run the next line to start again
-# django-admin startproject chriswmann /usr/src/app/chriswmann
-apt-get -y update && apt-get -y upgrade && apt-get -y install vim
-mkdir -vp analysis/data/tests
-touch analysis/data/__init__.py
-touch analysis/data/tests/__init__.py
